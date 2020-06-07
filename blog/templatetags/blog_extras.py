bf6dc7f3a019a92ff05c3be4bfa125ed36eb3430
Dominic Delabruere <ddelabru@redhat.com>
2020-06-07 03:03:48 -0400

    Remove unused datetime import

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,8 +2,10 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from markdown import markdown
 
+
 def smartypants_markdown(text):
     return markdown(text, extensions=["smarty"])
+
 
 register = template.Library()
 register.filter("markdown", stringfilter(smartypants_markdown))
