b543c526a0e74478853b0d306b86ecba2b21d10b
Dominic Delabruere <ddelabru@redhat.com>
2020-06-08 01:56:27 -0400

    Add blog RSS feed

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("article/<slug:slug>/", views.article_view, name="article"),
     path("tag/<str:tag_name>/", views.tag_view, name="tag"),
     path("tag/<str:tag_name>/page/<int:page_number>/", views.tag_view, name="tag_page"),
+    path("rss/", views.ArticlesFeed(), name="rss"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from django.contrib.syndication.views import Feed
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils.feedgenerator import Rss201rev2Feed
 from .models import Article, Tag
 
 
@@ -52,3 +55,37 @@ def tag_view(request, tag_name="", page_number=1):
         )
     else:
         raise Http404("Invalid page number")
+
+
+class ArticlesFeed(Feed):
+    title = "hamster.dance blog articles"
+    link = "/blog/"
+    description = "Articles from the hamster.dance blog."
+    feed_url = "/blog/rss/"
+    author_name = "Dominique Cyprès"
+    author_email = "lunasspecto@hamster.dance"
+    feed_type = Rss201rev2Feed
+
+    def items(self):
+        return Article.objects.filter(published__lte=datetime.now()).order_by(
+            "-published"
+        )
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        published_date_string = item.published.strftime("%d %b %Y")
+        return f"Blog article published {published_date_string}."
+
+    def item_link(self, item):
+        return reverse("blog:article", args=[item.slug])
+
+    def item_pubdate(self, item):
+        return item.published
+
+    def item_updateddate(self, item):
+        return item.modified
+
+    def item_categories(self, item):
+        return [tag.name for tag in item.tags.all()]
